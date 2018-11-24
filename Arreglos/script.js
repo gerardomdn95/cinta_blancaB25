d1d1c8ae62279dc608b8c216bf9b85e2a5c72ddd
gerardomdn95 <gerardo.mdn95@gmail.com>
2018-11-23 21:47:00 -0600

    clase 23 nov

---
+++ Arreglos/script.js
@@ -0,0 +1,45 @@
+// //              0       1       2       3
+// // let autos = ['Volvo', 'Ford', 'Seat', 'Nissan'];
+
+// let i = 0;
+// let cervezas = [3];
+
+// for(i; i <= 3; i++) {
+//     cervezas[i] = prompt('Ingresa tu cerveza');
+// }
+
+// console.log(cervezas);
+
+// // Arreglo 10 elementos
+
+// arreglo = ['2', '4', '6', '8' ...];
+
+// // For ?
+// // While ?
+// // ¿Qué necesito? 🤷🏽‍♂️
+
+// // REVISEN PROGRAMAS ANTERIORES
+
+// let num = 2;
+// let resultado;
+
+// resultado = num % 2;
+
+
+let pares = [];
+let i = 0;
+let e = 9;
+
+while(e <= 9) {
+    if(i % 2 === 0 && i != 0)  {
+        pares[e] = i;
+        e++;
+    }
+    i++;
+}
+
+console.log(pares);
+
+// pares = [2, ]
+// e = 1;
+// i = 3;
