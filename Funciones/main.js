b4eb17aca9ff6f43faaedcbe35b18242936aedc0
Gerardo Medina <gerardo.mdn95@gmail.com>
2018-11-29 21:52:42 -0600

    funciones

---
+++ Funciones/main.js
@@ -0,0 +1,31 @@
+// Función
+
+'use strict'
+
+function sumar() {
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+
+    console.log(parseInt(numero1) + parseInt(numero2));
+}
+
+function restar() {
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+
+    console.log(parseInt(numero1) - parseInt(numero2));
+}
+
+function imprimir() {
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+
+    console.log(numero1);
+    console.log(numero2);
+}
+
+
+// Ejercicio1 calcular Velocidad de un objeto.
+// Ejercicio2 calcular aceleración de un objeto
+
+// inner 
