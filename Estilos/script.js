750d4b502b6b74895c2bd8f4d8c4cd50cd9db5cd
gerardomdn95 <gerardo.mdn95@gmail.com>
2019-01-20 20:15:18 -0600

    Evidencia clases batch 25

---
+++ Estilos/script.js
@@ -0,0 +1,8 @@
+// Piedra Papel Tijeras
+
+// Usar Bootstrap inputs
+// Color Rojo J1 (innerHTML)
+// Color Azul J2 (innerHTML)
+
+// Usar funciones vinculadas a un botón que diga jugar
+// (onclick)
